0c3aa2fe4f97e9198ba9e71ca922dfe5a5c6ce0c
Diego-Lara-UCA <00138921@uca.edu.sv>
2024-05-26 18:10:06 -0600

    Fix: font size

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,16 +4,16 @@ const Home = () =>{
 
     return (
         <div>
-            <header className="header-home">
-                <image className="header-logo">HLVS</image>
-                <a className="header-login-btn" href="#">LOG IN</a>  
+            <header className="header_home">
+                <h1 className="header_logo">HLVS</h1>
+                <a className="header_login_btn" href="#">LOG IN</a>  
             </header>
             <div className="container">
-                <div className="home-container">
-                    <h1 className="div-title-text">Residential Security System</h1>
-                    <h3 className="div-sub-text">Take control of your residential security with our innovative access control system</h3>
-                    <div className="btn-container">
-                        <button className="div-get-started-btn">GET STARTED</button>
+                <div className="home_container">
+                    <h1 className="div_title_text">Residential Security System</h1>
+                    <h3 className="div_sub_text">Take control of your residential security with our innovative access control system</h3>
+                    <div className="btn_container">
+                        <button className="div_get_started_btn">GET STARTED</button>
                     </div>
                 </div>
             </div>
