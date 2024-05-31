5ea9fe7d2d462e797509821a636f8fafda369c0b
ocruzmartz <00109321@uca.edu.sv>
2024-05-31 09:42:10 -0600

    Update:  added user managed routes

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -4,16 +4,39 @@ import LogIn from "../pages/login/LogIn";
 import Home from "../pages/Home/Home";
 import Dashboard from "../pages/dashboard/Dashboard";
 import LogsEntries from "../pages/logofentries/LogOfEntries";
+import PermissionsDetails from "../pages/permissionsdetails/PermissionsDetails";
+import GenerateKeys from "../pages/generatekeys/GenerateKeys";
+import { userRole } from "../components/sidebar/userRole";
+import Profile from "../pages/profile/Profile";
 
 const AppRouter = () => {
+  const getInitialRoute = (userRole) => {
+    switch (userRole) {
+      case "admin":
+        return "logofentries";
+      case "superadmin":
+        return "superadmin";
+      case "user":
+        return "logofentries";
+      default:
+        return "logofentries";
+    }
+  };
+
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LogIn />} />
-        <Route path="/dashboard" element={<Dashboard />}>
-          <Route path="" element={<Navigate to="logofentries" />} />
+        <Route path="/dashboard/*" element={<Dashboard />}>
+          <Route
+            path=""
+            element={<Navigate to={getInitialRoute(userRole)} />}
+          />
           <Route path="logofentries" element={<LogsEntries />} />
+          <Route path="permissiondetails" element={<PermissionsDetails />} />
+          <Route path="generatekeys" element={<GenerateKeys />} />
+          <Route path="dashboard/profile" element={<Profile />} />
         </Route>
       </Routes>
     </div>
