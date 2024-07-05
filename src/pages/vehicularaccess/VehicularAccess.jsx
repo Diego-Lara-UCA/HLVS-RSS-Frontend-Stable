ba4ca8ec47ec03f417a0993afce32a31ec58c1e8
Omi1212 <00066621@uca.edu.sv>
2024-07-04 19:36:48 -0600

    Add: MQTT connection as a service

--- src/pages/vehicularaccess/VehicularAccess.jsx
+++ src/pages/vehicularaccess/VehicularAccess.jsx
@@ -1,15 +1,14 @@
 import React, { useRef, useEffect, useState } from "react";
 import Title from "../../components/title/Title";
 import QrScanner from "qr-scanner";
-import mqtt from "mqtt";
+import useMqttConnection from "../../services/mqttConnection";
 
 const VehicularAccess = () => {
   const scanner = useRef();
   const videoEl = useRef(null);
   const qrBoxEl = useRef(null);
   const [qrOn, setQrOn] = useState(true);
-  const [client, setClient] = useState(null);
-  const [connectStatus, setConnectStatus] = useState('Conectando...');
+  const { connectStatus, publishMessage } = useMqttConnection();
 
   const onScanSuccess = (result) => {
     // Imprime el resultado en la consola del navegador
@@ -55,58 +54,6 @@ const VehicularAccess = () => {
     };
   }, []);
 
-  useEffect(() => {
-    const connectUrl = 'wss://d326e3e9.ala.dedicated.aws.emqxcloud.com:8084/mqtt';
-    const options = {
-      keepalive: 30,
-      clientId: `mqtt_${Math.random().toString(16).slice(3)}`,
-      protocolId: 'MQTT',
-      protocolVersion: 4,
-      clean: true,
-      reconnectPeriod: 1000,
-      connectTimeout: 30 * 1000,
-      username: 'adminHLVS',
-      password: 'oscarin777',
-      will: {
-        topic: 'WillMsg',
-        payload: 'Connection Closed abnormally..!',
-        qos: 0,
-        retain: false
-      },
-      rejectUnauthorized: false
-    };
-
-    const mqttClient = mqtt.connect(connectUrl, options);
-    mqttClient.on('connect', () => {
-      setConnectStatus('Conectado');
-    });
-    mqttClient.on('error', (err) => {
-      console.error('Error de conexión:', err);
-      setConnectStatus('Error de conexión');
-      mqttClient.end();
-    });
-
-    setClient(mqttClient);
-
-    return () => {
-      mqttClient.end();
-    };
-  }, []);
-
-  const publishMessage = (topic, message) => {
-    if (client) {
-      client.publish(topic, message, { qos: 0, retain: false }, (error) => {
-        if (error) {
-          console.log('Error al publicar:', error);
-        } else {
-          console.log(`Mensaje publicado al tópico ${topic}: ${message}`);
-        }
-      });
-    } else {
-      console.log('No conectado a MQTT');
-    }
-  };
-
   return (
     <div className="container-tab">
       <Title
@@ -129,6 +76,7 @@ const VehicularAccess = () => {
         </div>
       </div>
 
+      <h2>Estado de la Conexión MQTT: {connectStatus}</h2>
     </div>
   );
 };
