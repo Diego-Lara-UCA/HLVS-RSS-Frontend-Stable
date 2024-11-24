f42872aa0dde5043cc2a12da284c741bfa845d48
Kemc17 <00129421@uca.edu.sv>
2024-11-24 17:39:06 -0600

    Add: new notification module :sparkles:

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
-import { NavLink } from "react-router-dom";
+import NotificationsButton from "../../components/notificationbutton/NotificationButton"
 
-const Navbar = ({ toggleMenu }) => {
+const Navbar = ({ toggleMenu }: { toggleMenu: () => void }) => {
   return (
     <nav className="flex w-full items-center justify-between p-4 2xl:pr-10 shadow-md sticky top-0 z-20 bg-white">
+      {/* Botón del menú */}
       <div>
         <button onClick={toggleMenu}>
           <MenuRoundedIcon fontSize="large" className="text-gray-600" />
         </button>
       </div>
-      {/* <div className="flex justify-center items-center">
-        <NavLink
-          to="/dashboard/profile"
-          className="bg-gray-200 rounded-full w-8 h-8 flex justify-center items-center hover:bg-gray-300"
-        >
-        
-        </NavLink>
-      </div> */}
+
+      {/* Botón de notificaciones */}
+      <div className="ml-auto relative">
+        <NotificationsButton />
+      </div>
     </nav>
   );
 };
